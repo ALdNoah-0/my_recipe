a8f2f6a894e1483bec41dcd735e370d4a4a53ffb
Kyle Sanchez <kylesanchez3000@gmail.com>
2026-04-09 15:14:33 +0800

    fix: Used HashRouter instead of BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
 import RecipePage from './pages/RecipePage';
